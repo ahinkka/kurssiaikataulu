34f5e73a7f37f3abf8b7006a11b0f744d2c80d6d
Atte Hinkka <atte.hinkka@iki.fi>
2014-12-03 21:26:51 +0200

    Comments

--- kurssiaikataulu.py
+++ kurssiaikataulu.py
@@ -1,10 +1,13 @@
 import sys
+import defaultdict
 
+# Basic data structures
 students = {}
 students_total = None
 courses_total = None
 students_per_course = None
 
+# Read input from stdin, first line requires special handling
 linenum = 0
 for line in sys.stdin:
     if linenum == 0:
@@ -18,6 +21,7 @@ for line in sys.stdin:
 
         students[student] = (course_count, courses)
 
+# Print out sanity check statistics
 print(students_total, courses_total, students_per_course)
 print(len(students))
 
@@ -26,3 +30,5 @@ for student in students.keys():
     course_count, _ = students[student]
     courses_sum += course_count
 print(float(courses_sum) / float(len(students)))
+
+    
