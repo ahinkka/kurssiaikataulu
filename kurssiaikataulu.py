592a522fa6a0774f06e0581d91b72fb36ff65c46
Atte Hinkka <atte.hinkka@iki.fi>
2014-12-03 21:23:45 +0200

    Initial commit, read input

---
+++ kurssiaikataulu.py
@@ -0,0 +1,28 @@
+import sys
+
+students = {}
+students_total = None
+courses_total = None
+students_per_course = None
+
+linenum = 0
+for line in sys.stdin:
+    if linenum == 0:
+        students_total, courses_total, students_per_course = (int(part) for part in line.strip().split(' '))
+        linenum += 1
+    else:
+        parts = line.strip().split(' ')
+        student = parts[0]
+        course_count = int(parts[1])
+        courses = parts[2:]
+
+        students[student] = (course_count, courses)
+
+print(students_total, courses_total, students_per_course)
+print(len(students))
+
+courses_sum = 0
+for student in students.keys():
+    course_count, _ = students[student]
+    courses_sum += course_count
+print(float(courses_sum) / float(len(students)))
