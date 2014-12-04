ea0cc1c8473b3e1bc96f4f33f155966657bf86f2
Atte Hinkka <atte.hinkka@iki.fi>
2014-12-04 20:07:40 +0200

    Similarity based

--- kurssiaikataulu.py
+++ kurssiaikataulu.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import collections
 import operator
+import heapq
 
 import bitsets
 
@@ -46,11 +47,6 @@ for student in students.keys():
     courses_sum += course_count
 debug(float(courses_sum) / float(len(students)))
 
-
-#
-# Course popularity; handle courses in popularity order, assign in loop to
-# spots
-#
 course_applicant_count = collections.defaultdict(int)
 for student in students.keys():
     course_count, courses = students[student]
@@ -62,14 +58,6 @@ for student in students.keys():
 applicant_counts = sorted(list(course_applicant_count.items()),
                           key=operator.itemgetter(1), reverse=True)
 
-# current_spot = 1
-# for course, count in applicant_counts:
-#     print(course, current_spot)
-#     current_spot += 1
-
-#     if current_spot > 25:
-#         current_spot = 1
-
 course_applicants = collections.defaultdict(list)
 for student in students.keys():
     course_count, courses = students[student]
@@ -80,56 +68,45 @@ course_applicant_sets = {}
 for course, applicants in course_applicants.items():
     course_applicant_sets[course] = Students(course_applicants[course])
 
-courses_left = set(all_courses)
+# First determine most similar courses to the 25 biggest courses
+initial_courses = [c[0] for c in applicant_counts[0:25]]
+courses_left = set(all_courses) - set(initial_courses)
+debug(len(initial_courses), len(courses_left))
+similar_courses = collections.defaultdict(list)
+
 current_spot = 1
-previous_spots = collections.defaultdict(set)
 while len(courses_left) > 0:
     if len(courses_left) % 100 == 0:
         debug(u"{0}/{1}".format(len(courses_left), len(all_courses)))
 
-    previous_courses_on_the_spot = previous_spots[current_spot]
-
-    # Go through all courses left to find out with best score. Score compared
-    # to a single course is the applicant set overlap of the courses
-    # subtracted from the applicant set size of the candidate course.
-    #
-    # I.e. CANDIDATE-COURSE-APPLICANT-COUNT -
-    #      SET-OVERLAP(CANDIDATE_COURSE-APPLICANTS,
-    #                  CURRENT-SPOT-PREVIOUS-ROUND-APPLICANTS).
-    #
-    # The compound score for all the previous courses on the spot is the sum
-    # of individual scores.
-    best_score = None
-    best_score_course = None
-
+    heap = []
+    initial_course = initial_courses[current_spot - 1]
     for course, applicant_count in applicant_counts:
         if course not in courses_left:
             continue
 
-        score_sum = 0
-        for previous_course in previous_courses_on_the_spot:
-            previous_course_applicants = course_applicant_sets[previous_course]
+        set_overlap = \
+            course_applicant_sets[initial_course].intersection(course_applicant_sets[course]).__len__()
+        heapq.heappush(heap, (1000 - set_overlap, course))
 
-            set_overlap = \
-                course_applicant_sets[course].intersection(course_applicant_sets[previous_course]).__len__()
-            score = applicant_count - set_overlap
-
-            score_sum += score
-            
-        if best_score is None or score_sum > best_score:
-            best_score = score_sum
-            best_score_course = course
-
-    assert best_score_course is not None
-
-    previous_spots[current_spot].add(best_score_course)
-    courses_left.remove(best_score_course)
-    print(best_score_course, current_spot)
+    best = heapq.heappop(heap)[1]
+    similar_courses[current_spot].append(best)
+    courses_left.remove(best)
 
     current_spot += 1
     if current_spot > 25:
         current_spot = 1
 
+for index, course in enumerate(initial_courses):
+    print(course, index + 1)
 
-# for course, count in applicant_counts:
-#     print(course, course_applicant_sets[course])
+for spot in similar_courses:
+    current_spot = 1
+    courses = similar_courses[spot]
+
+    for course in courses:
+        print(course, current_spot)
+
+        current_spot += 1
+        if current_spot > 25:
+            current_spot = 1
