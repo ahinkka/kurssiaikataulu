853e60aba4902de3e03114aa7c06d7acf030d751
Atte Hinkka <atte.hinkka@iki.fi>
2014-12-27 00:05:55 +0200

    Scoring should now work

--- score.jl
+++ score.jl
@@ -2,8 +2,8 @@
 
 @assert length(ARGS) == 1 "Original course file required as the first argument to script"
 
-participant_courses = Dict{ASCIIString,Vector{ASCIIString}}()
-course_participants = Dict{ASCIIString,Vector{ASCIIString}}()
+participant_courses = Dict{Int64,Vector{Int64}}()
+course_participants = Dict{Int64,Vector{Int64}}()
 f = open(ARGS[1])
 for (index, line) in enumerate(eachline(f))
     if index == 1
@@ -12,11 +12,11 @@ for (index, line) in enumerate(eachline(f))
     end
 
     parts = split(strip(line, '\n'), [' '])
-    course = parts[1]
-    participants = parts[3:end]
-    @assert length(participants) == uint16(parts[2])
+    participant = int64(parts[1])
+    courses = int64(parts[3:end])
+    @assert length(courses) == uint16(parts[2])
 
-    for participant in participants
+    for course in courses
         if ! haskey(course_participants, course) course_participants[course] = [] end
         push!(course_participants[course], participant)
 
@@ -33,51 +33,38 @@ course_count = length(keys(participant_courses))
 info("Total courses: $course_count")
 
 
-slots = Dict{ASCIIString,ASCIIString}()
+slots = Dict{Int64,Int64}()
 for line in eachline(STDIN)
     parts = split(strip(line, '\n'), [' '])
-    slots[parts[1]] = parts[2]
+    slots[int64(parts[1])] = int64(parts[2])
 end
 
-@assert length(keys(slots)) == course_count "No slots for all courses"
+# @assert length(keys(slots)) == course_count "No slots for all courses"
+if length(keys(slots)) != course_count
+    slot_count = length(keys(slots))
+    warn("No slots for all courses: length(keys(slots)): $slot_count != $course_count")
+end
 
 score = uint32(0)
-# for (student, student_courses) in courses
-#     slots_used = Set{ASCIIString}()
-#     for course in student_courses
-#         if ! haskey(slots, course)
-#             warn("Course $course not in slots")
-#             continue
-#         end
-
-#         course_slot = slots[course]
-#         if ! in(slots_used, course_slot)
-#             score = score + 1
-#             push!(slots_used, course_slot)
-#         end
-#     end
-# end
-
-user_slot_used = Dict{ASCIIString,Set{ASCIIString}}() # user => set of slots used for user
-for (course, participants) in course_participants
-    # if ! haskey(slots, course)
-    #     warn("Course $course not in slots")
-    #     continue
-    # end
-
-    slot = slots[course]
-    for participant in participants
-        if ! haskey(user_slot_used, participant)
-            user_slot_used[participant] = Set{ASCIIString}()
-        end
-
-        if in(user_slot_used[participant], slot)
+not_on_slots = uint32(0)
+for (participant, courses) in participant_courses
+    slots_used = IntSet()
+    for course in courses
+        if ! haskey(slots, course)
+            not_on_slots = not_on_slots + 1
             continue
         end
 
-        score = score + 1
-        push!(user_slot_used[participant], slot)
+        course_slot = slots[course]
+        if ! in(slots_used, course_slot)
+            score = score + 1
+            push!(slots_used, course_slot)
+        end
     end
+end
+
+if not_on_slots > 0
+    warn("$not_on_slots courses not designated to any slot")
 end
 
 println(score)
